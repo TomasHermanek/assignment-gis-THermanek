61c20824befb21d08cd233db437f6ecf53050005
tomhermanek@gmail.com <tomhermanek@gmail.com>
2016-10-06 23:32:06 +0200

    Added support for handling different routes

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
     require_once __DIR__ . '/vendor/autoload.php';
 
-    use \Database\PostgressDb;
-    use \Controller\WtfController;
-    use \Repository\WtfRepository;
+    use Database\PostgressDb;
+    use Controller\WtfController;
+    use Repository\WtfRepository;
+    use Core\Routing;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
 
     $str = file_get_contents('config/parameters.json');
     $json = json_decode($str, true);
@@ -11,8 +14,25 @@
     $database = new PostgressDb($json['host'], $json['port'], $json['dbName'], $json['usernameName'], $json['userPassword']);
     $database->connect();
 
-    $wtfRepository = new \Repository\WtfRepository();
-    $controller = new \Controller\WtfController($database, $wtfRepository);
-    $controller->getAllWtfPointsAction();
+    $wtfRepository = new WtfRepository();
+    $controller = new WtfController($database, $wtfRepository);
+    $result = $controller->getAllWtfPointsAction();
+
+    $request = Request::createFromGlobals();
+
+    $router = new Routing();
+    try {
+        $routeParameters = $router->matchRoute($request);
+        $objectString = 'Controller\\'.$routeParameters['controller'];
+        $object = new $objectString;
+        $object->{$routeParameters['action']}($request);
+    }
+    catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $exception) {
+        $response = new Response();
+        $response->prepare($request);
+        $response->setContent('Not Found');
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        $response->send();
+    }
 
     $database->disconnect();
